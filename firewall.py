f1ec5a7f89142e891208ef79b715b0cb1a8bebea
g.bychkov <g.bychkov@grcc.tech>
2021-03-08 14:51:15 +0300

    WIP

--- firewall.py
+++ firewall.py
@@ -1,6 +1,13 @@
 import socket
 import threading
 import time
+import queue
+
+# Буфер
+buffer_synchronizing = queue.Queue()
+buffer_covert_sending = queue.Queue()
+is_synchronizing = queue.Queue()
+is_covert_sending = queue.Queue()
 
 
 # Имитация сетевого экрана
@@ -8,7 +15,12 @@ def listener():
     while True:
         data, address = sor.recvfrom(1024)
         if address[1] == 5001:
-            sor.sendto(data, SECOND_CLIENT_ADDRESS)
+            if is_synchronizing.empty() and is_covert_sending.empty():
+                sor.sendto(data, SECOND_CLIENT_ADDRESS)
+            elif not is_synchronizing.empty():
+                buffer_synchronizing.put_nowait(data)
+            elif not is_covert_sending.empty():
+                buffer_covert_sending.put_nowait(data)
         if address[1] == 5002:
             sor.sendto(data, FIRST_CLIENT_ADDRESS)
 
@@ -16,22 +28,39 @@ def listener():
 def synchronizer():
     counter = 0
     while True:
-        sor.sendto(''.encode('utf-8'), SECOND_CLIENT_ADDRESS)
-        counter += 1
-        time.sleep(1)
+        if buffer_synchronizing.empty():
+            sor.sendto(''.encode('utf-8'), SECOND_CLIENT_ADDRESS)
+            counter += 1
+            time.sleep(1)
+        else:
+            sor.sendto(buffer_synchronizing.get_nowait(), SECOND_CLIENT_ADDRESS)
+            counter += 1
+            time.sleep(1)
         if counter == 5:
             break
 
 
 def coverts_sender(covert_message):
+    is_synchronizing.put_nowait(1)
     synchronizer()
+    useless = is_synchronizing.get_nowait()
+    is_covert_sending.put_nowait(1)
     lst = list(covert_message)
     for index in range(len(lst)):
         if lst[index] == '1':
-            sor.sendto(''.encode('utf-8'), SECOND_CLIENT_ADDRESS)
-            time.sleep(1)
+            if buffer_covert_sending.empty():
+                sor.sendto(''.encode('utf-8'), SECOND_CLIENT_ADDRESS)
+                time.sleep(1)
+            else:
+                sor.sendto(buffer_covert_sending.get_nowait(), SECOND_CLIENT_ADDRESS)
+                time.sleep(1)
         if lst[index] == '0':
             time.sleep(1)
+    while not buffer_synchronizing.empty():
+        sor.sendto(buffer_synchronizing.get_nowait(), SECOND_CLIENT_ADDRESS)
+    while not buffer_covert_sending.empty():
+        sor.sendto(buffer_covert_sending.get_nowait(), SECOND_CLIENT_ADDRESS)
+    more_useless = is_covert_sending.get_nowait()
 
 
 # Параметры сокета
